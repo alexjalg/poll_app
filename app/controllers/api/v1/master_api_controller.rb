f6bcf3d2024e6ec009ccb3485bce81734ca73b2c
alexjalg <alexjalg@gmail.com>
2016-03-29 05:34:11 +0000

    CORS Headers

--- app/controllers/api/v1/master_api_controller.rb
+++ app/controllers/api/v1/master_api_controller.rb
@@ -1,3 +1,11 @@
 class Api::V1::MasterApiController < ApplicationController
     layout "api/v1/application"  
+    after_action :cor_set_access_control_headers
+    
+    def cor_set_access_control_headers
+        #headers['Access-Control-Allow-Origin'] = "https://angular-js-alexjalg.c9.io"
+        headers['Access-Control-Allow-Origin'] = "*"
+        headers['Access-Control-Allow-Methods'] = "POST,GET,DELETE,OPTIONS"
+        headers['Access-Control-Allow-Headers'] = "Origin,Content-Type,Accept,Authorization,Token"
+    end
 end
